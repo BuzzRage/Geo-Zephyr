580e7eab6b237ab07d95dfc628fd9bba5e7bb005
BuzzRage <marius95@live.fr>
2019-04-17 20:51:58 +0200

    test de l'exemple GPS + pin mapping pour GPS + Flag de désactivation WiFi pour debug

--- include/global.h
+++ include/global.h
@@ -13,14 +13,24 @@
 #endif
 
 #define SERIAL_SPEED 		      9600
-#define WEBSERVER_PORT				80
-
-// Defines peripheral pins
+//#define WIFI_ENABLED
+#ifdef WIFI_ENABLED
+	#define WEBSERVER_PORT				80
+#endif
+/* ---------- Defines peripheral pins ------------------------ */
+// Air Quality control
 #define BME_VCC               3V3   // 3.3v
 #define BME_GND               GND   // GND
 #define BME_SDA               SDA   // 21
 #define BME_SCL               SCL   // 22
 #define I2C_ADDR							0x76  // I2C Address for Sparkun ESP32 Thing
+
+//GPS
+#define GPS_VCC								3V3
+#define GPS_GND								GND
+#define GPS_TX								16
+#define GPS_RX								17
+
 
 #define SEALEVELPRESSURE_HPA  1013.25
 
